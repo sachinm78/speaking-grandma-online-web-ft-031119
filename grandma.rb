cc336e3ac67dca48a8e56df621971eca0d06e8d9
Sachin Mahajan <sachinm78@gmail.com>
2019-03-11 18:18:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,9 +17,9 @@ def speak_to_grandma(phrase)
       return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.downcase
   		return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.downcase
-      return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
-
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
 end
+
